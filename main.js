1d08c11fc662638b3c828e0033c0abcfb8610335
Key Estanga <keylinestanga@gmail.com>
2017-06-26 13:13:05 +0200

    Exercise header finished

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+var toggleMenu = function (event) {
+  var menu = document.getElementById('js-menu-small');
+  if (menu.classList.contains('hide')) {
+    menu.classList.remove('hide');
+  } else {
+    menu.classList.add('hide');
+  }
+};
+
+document.getElementById('js-button-burguer').addEventListener('click', toggleMenu);
